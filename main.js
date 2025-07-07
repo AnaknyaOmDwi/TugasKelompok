3e9b8cc99e7b24b00f7e2c46bedcc87df03bc66b
AnaknyaOmDwi <rbyas2234@gmail.com>
2025-07-07 16:09:54 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const publicPath = path.join(process.cwd(), 'public');
 
 router.get('/',(req,res)=>{
     res.sendFile(__dirname+'/public/cart.html')
